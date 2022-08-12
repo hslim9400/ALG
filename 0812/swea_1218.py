e1ce1848cb984757d3e600f42025d5292c596fd6
hslim9400 <hslim9400@gmail.com>
2022-08-12 09:32:12 +0900

    0812 alg2

--- 0812/swea_1218.py
+++ 0812/swea_1218.py
@@ -4,8 +4,8 @@ for test_case in range(1, 11):
     prob = input()
     ans = 0
 
-    for i in range(len(prob) - len(target) + 1):
-        if prob[i:i + len(target)] == target:
+    for i in range(len(prob) - len(target) + 1):  # 특정 문자열의 길이만큼 확인할 것이므로
+        if prob[i:i + len(target)] == target:  # 문자열의 길이만큼 슬라이싱하여 확인
             ans = ans + 1
 
     print(f'#{test_case} {ans}')
--- 0812/swea_1221.py
+++ 0812/swea_1221.py
@@ -3,22 +3,22 @@ T = int(input())
 for test_case in range(1, T + 1):
     case, N = input().split()
     N = int(N)
-    nums = list(range(10))
-    alien = ['ZRO', 'ONE', 'TWO', 'THR', 'FOR', 'FIV', 'SIX', 'SVN', 'EGT', 'NIN']
+    nums = list(range(10))  # 0부터 9까지의 숫자리스트
+    alien = ['ZRO', 'ONE', 'TWO', 'THR', 'FOR', 'FIV', 'SIX', 'SVN', 'EGT', 'NIN']  # 0부터 9까지 외계어리스트
     signal = input().split()
-    trans = []
-    decoder = {}
+    trans = []  # 변환된 것을 담을 리스트
+    decoder = {}  # 서로 변환하게 할 딕셔너리
 
-    for i in range(10):
+    for i in range(10):  # 숫자와 외계어를 매칭할 딕셔너리 생성
         decoder[nums[i]] = alien[i]
         decoder[alien[i]] = nums[i]
 
-    for i in range(N):
+    for i in range(N):  # 외계어를 숫자로 변환해 리스트에 저장
         trans.append(decoder[signal[i]])
 
-    trans.sort()
-    for i in range(N):
+    trans.sort()  # 리스트를 정렬
+    for i in range(N):  # 숫자를 외계어로 변환
         trans[i] = decoder[trans[i]]
 
     print(case)
-    print(*trans)
+    print(*trans)  # 애스터리스크로 언패킹하여 출력
