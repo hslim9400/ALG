de5fabec23119ea8afc6988b25a6c3e91b531c1f
hslim9400 <hslim9400@gmail.com>
2022-08-29 08:30:42 +0900

    0828 3

--- 0828/boj_17136.py
+++ 0828/boj_17136.py
@@ -1,38 +1,41 @@
 def cover_one(ones, used):
     global board, papers, min_used
     if ones == 0:
+        print(used)
         if used < min_used:
             min_used = used
         return
 
-    for size in range(5, 0, -1):
-        if papers[size] == 0:
-            continue
-        for i in range(10-size+1):
-            for j in range(10-size+1):
-                if board[i][j] == 1:
-                    flag = False
-                    for i_check in range(size):
-                        for j_check in range(size):
-                            if board[i+i_check][j+j_check] == 0:
-                                flag = True
+    for i in range(10):
+        for j in range(10):
+            if board[i][j] == 1:
+                for size in range(1, 6):
+                    if i+size < 10 and j+size < 10 and papers[size] != 0:
+                        flag = False
+                        for i_check in range(size):
+                            for j_check in range(size):
+                                if board[i+i_check][j+j_check] == 0:
+                                    flag = True
+                                    break
+                            if flag:
                                 break
-                        if flag:
-                            break
-                    else:
-                        used += 1
-                        papers[size] -= 1
-                        ones -= size**2
-                        for i_check in range(size):
-                            for j_check in range(size):
-                                board[i+i_check][j+j_check] = 0
-                        cover_one(ones, used)
-                        used -= 1
-                        papers[size] += 1
-                        ones += size**2
-                        for i_check in range(size):
-                            for j_check in range(size):
-                                board[i+i_check][j+j_check] = 1
+                        else:
+                            used += 1
+                            if used >= min_used:
+                                used -= 1
+                                return
+                            papers[size] -= 1
+                            ones -= size**2
+                            for i_check in range(size):
+                                for j_check in range(size):
+                                    board[i+i_check][j+j_check] = 0
+                            cover_one(ones, used)
+                            used -= 1
+                            papers[size] += 1
+                            ones += size**2
+                            for i_check in range(size):
+                                for j_check in range(size):
+                                    board[i+i_check][j+j_check] = 1
 
 
 papers = {1: 5, 2: 5, 3: 5, 4: 5, 5: 5}
