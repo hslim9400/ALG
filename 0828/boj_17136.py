0dd09bbbf505773228c80a3ad7f423b86e24e1b8
hslim9400 <hslim9400@gmail.com>
2022-08-29 22:41:00 +0900

    0829 4

--- 0828/boj_17136.py
+++ 0828/boj_17136.py
@@ -1,41 +1,43 @@
-def cover_one(ones, used):
+def cover_one(r, c, used):
     global board, papers, min_used
-    if ones == 0:
-        print(used)
+    if r == 10:
         if used < min_used:
             min_used = used
         return
 
-    for i in range(10):
-        for j in range(10):
-            if board[i][j] == 1:
-                for size in range(1, 6):
-                    if i+size < 10 and j+size < 10 and papers[size] != 0:
-                        flag = False
-                        for i_check in range(size):
-                            for j_check in range(size):
-                                if board[i+i_check][j+j_check] == 0:
-                                    flag = True
-                                    break
-                            if flag:
-                                break
-                        else:
-                            used += 1
-                            if used >= min_used:
-                                used -= 1
-                                return
-                            papers[size] -= 1
-                            ones -= size**2
-                            for i_check in range(size):
-                                for j_check in range(size):
-                                    board[i+i_check][j+j_check] = 0
-                            cover_one(ones, used)
-                            used -= 1
-                            papers[size] += 1
-                            ones += size**2
-                            for i_check in range(size):
-                                for j_check in range(size):
-                                    board[i+i_check][j+j_check] = 1
+    if c == 10:
+        cover_one(r+1, 0, used)
+        return
+
+    if board[r][c] == 0:
+        cover_one(r, c+1, used)
+        return
+
+    for size in range(1, 6):
+        if papers[size] == 0:
+            continue
+        if r+size >= 11 or c+size >= 11:
+            continue
+        flag = False
+        for i in range(size):
+            for j in range(size):
+                if board[r+i][c+j] == 0:
+                    flag = True
+                    break
+            if flag:
+                break
+        else:
+            used += 1
+            papers[size] -= 1
+            for i in range(size):
+                for j in range(size):
+                    board[r+i][c+j] = 0
+            cover_one(r, c+size, used)
+            used -= 1
+            papers[size] += 1
+            for i in range(size):
+                for j in range(size):
+                    board[r+i][c+j] = 1
 
 
 papers = {1: 5, 2: 5, 3: 5, 4: 5, 5: 5}
@@ -46,7 +48,7 @@ for _ in range(10):
     board.append(line)
     ones += line.count(1)
 min_used = 25
-cover_one(ones, 0)
+cover_one(0, 0, 0)
 
 if min_used == 25:
     ans = -1
