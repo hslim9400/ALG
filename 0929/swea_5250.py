cf23d5fa65cd33565f501915fc8919404a70490a
hslim9400 <hslim9400@gmail.com>
2022-09-29 22:28:49 +0900

    0929 3

--- 0929/swea_5250.py
+++ 0929/swea_5250.py
@@ -1,5 +1,31 @@
-
+from collections import deque
 
 T = int(input())
 for test_case in range(1, T+1):
     N = int(input())
+
+    board = []
+    for _ in range(N):
+        board.append(list(map(int, input().split())))
+
+    costs = [[1000*N*N]*N for _ in range(N)]
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+
+    costs[0][0] = 0
+    queue = deque()
+    queue.append((0, 0))
+
+    while queue:
+        current = queue.popleft()
+        r, c = current
+        for d in range(4):
+            if 0 <= r+dr[d] < N and 0 <= c+dc[d] < N:
+                cost = 1
+                if board[r][c] < board[r+dr[d]][c+dc[d]]:
+                    cost += abs(board[r+dr[d]][c+dc[d]] - board[r][c])
+                if costs[r+dr[d]][c+dc[d]] > costs[r][c] + cost:
+                    costs[r+dr[d]][c+dc[d]] = costs[r][c] + cost
+                    queue.append((r+dr[d], c+dc[d]))
+
+    print(f'#{test_case} {costs[N-1][N-1]}')
--- 0929/swea_5251.py
+++ 0929/swea_5251.py
@@ -1,1 +1,33 @@
+def dijkstra():
+    dist = [10*E] * (N+1)
+    dist[0] = 0
+    visited = [0] * (N+1)
+
+    for _ in range(N):
+        min_idx = -1
+        min_value = 10 * E
+
+        for i in range(N+1):
+            if not visited[i] and dist[i] < min_value:
+                min_idx = i
+                min_value = dist[i]
+
+        visited[min_idx] = 1
+
+        for i in range(N+1):
+            if not visited[i] and dist[i] > adj[min_idx][i] + dist[min_idx]:
+                dist[i] = adj[min_idx][i] + dist[min_idx]
+
+    return dist[N]
+
+
 T = int(input())
+for test_case in range(1, T+1):
+
+    N, E = map(int ,input().split())
+    adj = [[10*E]*(N+1) for _ in range(N+1)]
+    for _ in range(E):
+        start, end, weight = map(int, input().split())
+        adj[start][end] = weight
+
+    print(f'#{test_case} {dijkstra()}')
