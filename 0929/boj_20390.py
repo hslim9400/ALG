7a9dcaf57a9055b8e00fa811d10b4d7fd3167183
hslim9400 <hslim9400@gmail.com>
2022-09-29 17:54:12 +0900

    0929 2

--- 0929/boj_20390.py
+++ 0929/boj_20390.py
@@ -1,33 +1,33 @@
-def find_set(x):
-    if p[x] != x:
-        p[x] = find_set(p[x])
-    return p[x]
+import sys
+sys.setrecursionlimit(100001)
 
 
-def union(x, y):
-    p[find_set(y)] = find_set(x)
+def find_edge(node):
+    global visited, dists, total_dists
+    visited.add(node)
+    if len(visited) == N:
+        return
+
+    cand = (1 << 81, N+1)
+    for j in range(N):
+        if j in visited:
+            continue
+        if node < j:
+            dists[j] = min(dists[j], ((costs[node]*A + costs[j]*B) % C) ^ D)
+        else:
+            dists[j] = min(dists[j], ((costs[j] * A + costs[node] * B) % C) ^ D)
+        if cand[0] > dists[j]:
+            cand = (dists[j], j)
+    total_dists += cand[0]
+    find_edge(cand[1])
 
 
 N = int(input())
 A, B, C, D = map(int, input().split())
 costs = list(map(int, input().split()))
-edges = []
-p = list(range(N))
-for i in range(N-1):
-    for j in range(i+1, N):
-        dist = ((costs[i]*A + costs[j]*B) % C) ^ D
-        edges.append([dist, i, j])
-
-edges.sort()
-total_costs = 0
-selected = 0
-for dist, x, y in edges:
-    if find_set(x) != find_set(y):
-        union(x, y)
-        total_costs += dist
-        selected += 1
-    if selected == N-1:
-        break
-
-print(total_costs)
-
+large = 1 << 100
+dists = [large] * N
+visited = set()
+total_dists = 0
+find_edge(0)
+print(total_dists)
