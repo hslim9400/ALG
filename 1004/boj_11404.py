765daf53bf3ed1b42ee8484f2543296ed0da94ac
hslim9400 <hslim9400@gmail.com>
2022-10-06 22:48:55 +0900

    1006 1

--- 1004/boj_11404.py
+++ 1004/boj_11404.py
@@ -1,16 +1,16 @@
 def floyd_warshall():
-    dists = [[INF]*(n+1) for _ in range(n+1)]
+    dists = [[INF]*(n+1) for _ in range(n+1)]  # 무한대로 초기 설정
 
     for i in range(1, n+1):
         for j in range(1, n+1):
-            dists[i][j] = adj[i][j]
-            if i == j:
+            dists[i][j] = adj[i][j]  # 연결되어 있는 노드들을 이어준다.
+            if i == j:  # 나 -> 나는 거리가 0
                 dists[i][j] = 0
 
-
-    for k in range(1, n+1):
+    for k in range(1, n+1):  # i에서 j까지 갈 때 지나갈 수 있는 노드
         for i in range(1, n+1):
             for j in range(1, n+1):
+                # 해당 노드를 경유하는 것이 더 짧은 거리가 된다면 갱신
                 if dists[i][j] > dists[i][k] + dists[k][j]:
                     dists[i][j] = dists[i][k] + dists[k][j]
 
