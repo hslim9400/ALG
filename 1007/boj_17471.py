eff9d388b97affd22bd7581e6256fd1e437cc58d
hslim9400 <hslim9400@gmail.com>
2022-10-09 18:13:30 +0900

    1009 2

--- 1007/boj_17471.py
+++ 1007/boj_17471.py
@@ -1,17 +1,22 @@
-
-def find_p(x):
-    if not p[x] == x:
-        p[x] = find_p(p[x])
-    return p[x]
-
-
-def union(x, y):
-    p[find_p(x)] = find_p(y)
+def dfs(start, group):
+    visited = [0] * (N+1)
+    stack = [start]
+    counts = 0
+    while stack:
+        current = stack.pop()
+        if visited[current]:
+            continue
+        visited[current] = 1
+        counts += 1
+        for destination in group:
+            if adj[current][destination] and not visited[destination]:
+                stack.append(destination)
+    return counts
 
 
 N = int(input())
 
-cities = list(map(int, input().split()))
+cities = [0] + list(map(int, input().split()))
 adj = [[0]*(N+1) for _ in range(N+1)]
 for i in range(N):
     line = list(map(int, input().split()))
@@ -20,38 +25,29 @@ for i in range(N):
         adj[line[j]][i+1] = 1
 
 min_diff = 100 * N
-for i in range(1<<N):
+for i in range((1<<N)//2):
     group_a = set()
     group_b = set()
     p = list(range(N + 1))
     r = [0] * (N + 1)
+    a_start = 0
+    b_start = 0
     for j in range(N):
         if i & (1<<j):
-            group_a.add(j)
+            group_a.add(j+1)
+            a_start = j+1
         else:
-            group_b.add(j)
-    roots = set()
-    for x in group_a:
-        for y in group_a:
-            if adj[x][y]:
-                union(x, y)
-    for x in group_b:
-        for y in group_b:
-            if adj[x][y]:
-                union(x, y)
-    for j in range(1, N):
-        roots.add(find_p(j))
-    a_population = 0
-    b_population = 0
-    group_a = list(group_a)
-    group_b = list(group_b)
-    if len(roots) != 2:
+            group_b.add(j+1)
+            b_start = j+1
+    if not a_start or not b_start:
         continue
-    print(roots, group_a, group_b)
-    for k in range(len(group_a)):
-        a_population += cities[group_a[k]]
-    for k in range(len(group_b)):
-        b_population += cities[group_b[k]]
+    if len(group_a) != dfs(a_start, group_a) or len(group_b) != dfs(b_start, group_b):
+        continue
+    a_population, b_population = 0, 0
+    for area in group_a:
+        a_population += cities[area]
+    for area in group_b:
+        b_population += cities[area]
     diff = abs(a_population - b_population)
     if diff < min_diff:
         min_diff = diff
