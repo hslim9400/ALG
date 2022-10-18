3fdcf78d82a38d5872672c70f42b207bcec1896a
hslim9400 <hslim9400@gmail.com>
2022-10-18 14:33:24 +0900

    1018 2

--- 1017/boj_21611.py
+++ 1017/boj_21611.py
@@ -1,5 +1,5 @@
 from collections import deque
-def snail():
+def snail():  # 달팽이 찍기
     global board
     current = N * N - 1
     r, c = 0, 0
@@ -19,80 +19,85 @@ def snail():
         current -= 1
 
 
-def explode(marbles):
+def explode(marbles):  # 구슬이 폭발
     global points
-    while True:
-        targets = []
+    while True:  # 폭발하는 구슬이 없을 때 까지
+        targets = []  # 폭발하는 구슬을 담을 예정
         l = len(marbles)
         start = 0
         counts = 0
         for i in range(1, l):
-            if marbles[i] == marbles[i-1]:
+            if marbles[i] == marbles[i-1]:  # 이어지는 구슬을 카운트
                 counts += 1
             else:
                 if counts >= 4:
-                    targets.append([start, counts])
+                    targets.append([start, counts])  # start 인덱스 구슬부터 counts개가 터짐
                     start = i
                     counts = 1
                 else:
                     start = i
                     counts = 1
         if counts >= 4:
-            targets.append([start, counts])
-        if not targets:
+            targets.append([start, counts])  # 마지막을 반영하지 않았음
+        if not targets:  # 터질 구슬이 없다면 종료
             return marbles
-        pulled = 0
+        pulled = 0  # 인덱스를 당길 숫자.
         for target in targets:
             counts = target[1]
-            idx = target[0] - pulled
+            idx = target[0] - pulled  # start부터 터질건데 pulled만큼 이미 터졌음
             for _ in range(counts):
-                points += marbles[idx]
-                del marbles[idx]
-            pulled += counts
+                points += marbles[idx]  # 점수는 어떤 구슬이 터지는지에 따라
+                del marbles[idx]  # 구슬 하나 터짐
+            pulled += counts  # 터뜨린 만큼 다음의 인덱스를 당길것임
 
 
-def transform(marbles):
+def transform(marbles):  # 규칙에 따라 변화
     l = len(marbles)
     if l == 1:
         return [0]
-    new_marbles = [0] * N**2
+    new_marbles = [0] * N**2  # 미리 0으로 된 배열을 만듬
     counts = 1
-    pointer = 1
+    pointer = 1  # 어디를 채울지 정하는 포인터
     for i in range(2, l):
-        if marbles[i] == marbles[i-1]:
+        if marbles[i] == marbles[i-1]:  # 이어지는 구슬을 카운트
             counts += 1
-        if marbles[i] != marbles[i-1]:
-            new_marbles[pointer] = counts
+        if marbles[i] != marbles[i-1]:  # 구슬 변화
+            new_marbles[pointer] = counts  # 몇개가 나왔는지
             pointer += 1
-            new_marbles[pointer] = marbles[i-1]
+            new_marbles[pointer] = marbles[i-1]  # 무슨 구슬인지
             pointer += 1
-            if pointer == N**2:
+            if pointer == N**2:  # 이 이상은 채울 수 없으니 종료
                 return new_marbles
             counts = 1
-    new_marbles[pointer] = counts
+    new_marbles[pointer] = counts  # 이번에도 마지막을 반영하지 않았음
     pointer += 1
     new_marbles[pointer] = marbles[-1]
-    for i in range(N**2-1, -1, -1):
+    for i in range(N**2-1, -1, -1):  # 0 자르기
         if new_marbles[i]:
             new_marbles = new_marbles[:i+1]
             break
     return new_marbles
 
 
+# 구슬은 1차원 배열에 담아준다.
+# 지도에 달팽이를 찍어주고 지도의 해당 위치를 찾아가면
+# 구슬 배열의 인덱스로 찾아갈 수 있다.
+# 배열은 빈 공간이 없으므로 구슬을 없애면 자동으로 당긴다.
+
 N, M = map(int, input().split())
 shark = ((N-1)//2, (N-1)//2)
 board = [[0]*N for _ in range(N)]
-snail()
-marbles = [0]*N**2
+snail()  # 지도에 구슬 배열의 인덱스를 표기
+marbles = [0]*N**2  # 0으로된 배열 생성
 for i in range(N):
     line = list(map(int, input().split()))
     for j in range(N):
-        marbles[board[i][j]] = line[j]
-for i in range(N**2-1, -1, -1):
+        marbles[board[i][j]] = line[j]  # 지도를 통해 구슬 배열의 인덱스를 찾아가 기록
+for i in range(N**2-1, -1, -1):  # 0 자르기
     if marbles[i]:
         marbles = marbles[:i+1]
         break
-else:
+else:  
     marbles = [0]
 
 dr = [0, -1, 1, 0, 0]
@@ -100,13 +105,13 @@ dc = [0, 0, 0, -1, 1]
 points = 0
 for _ in range(M):
     d, dist = map(int, input().split())
-    for k in range(1, dist+1):
-        r, c = shark[0]+k*dr[d], shark[1]+k*dc[d]
-        idx = board[r][c]
-        if idx-k+1 > len(marbles)-1:
+    for k in range(1, dist+1):  # 거리에 따라 가서 지워줄 것
+        r, c = shark[0]+k*dr[d], shark[1]+k*dc[d]  # 상어로부터 정해진 방향으로 k만큼 가서
+        idx = board[r][c]  # 보드를 보고 어떤 구슬 배열의 인덱스인지 확인
+        if idx-k+1 > len(marbles)-1:  # 찾아갈 수 없는 인덱스
             break
-        del marbles[idx-k+1]
-    marbles = explode(marbles)
-    marbles = transform(marbles)
+        del marbles[idx-k+1]  # 앞의 k에서 이미 지워줬으니 지운 만큼 인덱스를 당겨 찾아감
+    marbles = explode(marbles)  # 터뜨리고
+    marbles = transform(marbles)  # 바꾸고
 print(points)
 
--- 1018/boj_21610.py
+++ 1018/boj_21610.py
@@ -1,16 +1,19 @@
 dr = [0, 0, -1, -1, -1, 0, 1, 1, 1]
 dc = [0, -1, -1, 0, 1, 1, 1, 0, -1]
 
-def move_cloud(d, s, clouds):
+def move_cloud(d, s, clouds):  # 구름 옮기기
     new_clouds = set()
-    while clouds:
+    while clouds:  # 구름을 하나씩 뽑아서 넣을 것.
         cloud = clouds.pop()
         r, c = cloud[0], cloud[1]
-        nr, nc = r+s*dr[d], c+s*dc[d]
+        nr, nc = r+s*dr[d], c+s*dc[d]  # 지도를 넘어가도 상관 없다. 놀랍게도.
+        # N보다 커질 경우 나머지 연산을 하면 당연히 넘어간 만큼 인덱스가 되고
+        # 0보다 작을 경우 -1, -2, ..이 되는데 이것도 나머지 연산을 하면
+        # 넘어간 만큼 인덱스가 된다.
         nr %= N
         nc %= N
-        new_clouds.add((nr, nc))
-    return new_clouds
+        new_clouds.add((nr, nc))  # 새로운 구름들에 넣어서
+    return new_clouds  # 반환
 
 
 N, M = map(int, input().split())
@@ -19,32 +22,32 @@ for i in range(N):
     line = list(map(int, input().split()))
     board.append(line)
 
-clouds = {(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)}
+clouds = {(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)}  # 원래 구름들의 자리
 for _ in range(M):
     d, s = map(int, input().split())
-    clouds = move_cloud(d, s, clouds)
+    clouds = move_cloud(d, s, clouds)  # 구름을 움직이고
 
     for cloud in clouds:
-        board[cloud[0]][cloud[1]] += 1
+        board[cloud[0]][cloud[1]] += 1  # 해당 자리들에 물을 1씩 공급
 
     for cloud in clouds:
         r, c = cloud[0], cloud[1]
-        for x, y in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
-            if 0 <= r+x < N and 0 <= c+y < N:
-                if board[r+x][c+y]:
-                    board[r][c] += 1
+        for x, y in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:  # 대각선을 보고
+            if 0 <= r+x < N and 0 <= c+y < N:  # 지도 안이라면
+                if board[r+x][c+y]:  # 물이 있다면
+                    board[r][c] += 1  # 물 복사 버그
     new_clouds = set()
     for i in range(N):
-        for j in range(N):
-            if board[i][j] >= 2:
-                if (i, j) not in clouds:
-                    new_clouds.add((i, j))
+        for j in range(N):  # 지도에
+            if board[i][j] >= 2:  # 물이 2 이상 있고
+                if (i, j) not in clouds:  # 구름이 있던 자리가 아니라면
+                    new_clouds.add((i, j))  # 구름이 된다.
                     board[i][j] -= 2
 
-    clouds = new_clouds
+    clouds = new_clouds  # 새 구름들로 세대교체
 
 water = 0
-for i in range(N):
+for i in range(N):  # 남은 물을 세준다.
     for j in range(N):
         water += board[i][j]
 print(water)
