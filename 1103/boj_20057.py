206337718d45ed663821d580d65f71b2e2321fd0
hslim9400 <hslim9400@gmail.com>
2022-11-04 13:30:15 +0900

    1104

--- 1103/boj_20057.py
+++ 1103/boj_20057.py
@@ -1,3 +1,18 @@
+def rotate(lattice):
+    lattice = list(zip(*lattice[::-1]))
+    return lattice[:]
+
+
+origin = [
+    [0, 0, 0.02, 0, 0],
+    [0, 0.1, 0.07, 0.01, 0],
+    [0.05, 0, 0, 0, 0],
+    [0, 0.1, 0.07, 0.01, 0],
+    [0, 0, 0.02, 0, 0]
+]
+lattices = [origin, rotate(origin), rotate(rotate(origin)), rotate(rotate(rotate(origin)))]
+
+
 def snail(N):
     board = [[0]*N for _ in range(N)]
     snail = []
@@ -19,11 +34,27 @@ def snail(N):
 
 
 def scatter(place, d):
-    global board
+    global board, ans
     dr = [0, -1, 0, 1]
     dc = [-1, 0, 1, 0]
-    r, c = place
-    targets = [()]
+    r, c = place[0] + dr[d], place[1] + dc[d]
+    target = board[r][c]
+    lattice = lattices[d]
+    for i in range(5):
+        for j in range(5):
+            ratio = lattice[i][j]
+            nr, nc = r-2+i, c-2+j
+            if 0 <= nr < N and 0 <= nc < N:
+                board[nr][nc] += int(board[r][c] * ratio)
+            else:
+                ans += int(board[r][c] * ratio)
+            target -= int(board[r][c] * ratio)
+
+    if 0 <= r+dr[d] < N and 0 <= c+dc[d] < N:
+        board[r+dr[d]][c+dc[d]] += target
+    else:
+        ans += target
+    board[r][c] = 0
 
 
 N = int(input())
@@ -31,8 +62,11 @@ board = []
 for _ in range(N):
     board.append(list(map(int, input().split())))
 snail = snail(N)
-print(snail)
+ans = 0
 for current in snail:
     place = current[0]
     d = current[1]
+    scatter(place, d)
+
+print(ans)
 
