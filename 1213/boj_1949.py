d54217c89e01ea583b317f9cfb08ab669fe70b8b
hslim9400 <hslim9400@gmail.com>
2022-12-17 10:09:59 +0900

    1217 1

--- 1213/boj_1949.py
+++ 1213/boj_1949.py
@@ -1,4 +1,9 @@
 from collections import deque
+# 아이디어: 트리DP
+# 퐁당퐁당보다 퐁당당퐁이 더 클 수 있다는 것을 기억
+# 자식 마을이 우수마을일 경우, 자식마을이 우수마을이 아니고 내가 우수마을일 경우, 자식마을도 우수마을이 아니고 나도 우수마을이 아닌경우
+# 세 가지 경우를 쌓아 올라간다. 주변에 우수마을이 하나도 없는 경우는 어차피 최댓값 선정에서 탈락하기 때문에
+# 따로 고려하지 않음.
 
 N = int(input())
 populations = [0] + list(map(int, input().split()))
@@ -12,7 +17,7 @@ parent = [0] * (N+1)
 children = [set() for _ in range(N+1)]
 stack = [1]
 visited = set()
-while stack:
+while stack:  # 1을 루트로 트리를 구성해줌
     current = stack.pop()
     if current in visited:
         continue
@@ -35,12 +40,15 @@ while queue:
     if children[current] or current == 1:
         continue
     result[current][0] += populations[current]
-    target = parent[current]
+    target = parent[current]  # 부모에 경우를 더할 것임.
     result[target][0] += max(result[current][1], result[current][2])
+    # 부모가 우수마을이면 자식은 반드시 우수마을이 아님.
     result[target][1] += result[current][0]
+    # 부모가 우수마을이 아니고 자식은 우수마을인 경우
     result[target][2] += max(result[current][0], result[current][2])
+    # 부모도 우수마을이 아니고 자식도 우수마을이 아닌 경우를 포함
     children[target].discard(current)
-    if not children[target]:
+    if not children[target]:  # 자식이 없다면 이 녀석의 부모로 올릴 준비가 완료됨.
         queue.append(target)
 result[1][0] += populations[1]
 print(max(result[1]))
--- 1215/boj_2306.py
+++ 1215/boj_2306.py
@@ -1,4 +1,7 @@
-
+# 냅색알고리즘과 유사함
+# memo[i][j]는 dna의 i번째 글자부터 j번째 글자까지 중 최대 KOI
+# 'at', 'gc'로 감싸져 있는 경우 2를 더해주고
+# 둘로 나눠 만들 수 있는 KOI 중 가장 큰 값으로 갱신해준다.
 
 dna = input()
 memo = [[0] * len(dna) for _ in range(len(dna))]
@@ -11,6 +14,7 @@ for k in range(1, len(dna)):  # k글자 만큼
             memo[i][i+k] += 2
 
         for m in range(1, k+1):
+            # 어떤 부분문자열을 m개를 기준으로 두개로 나눠 가능한 가장 큰 값으로 갱신.
             memo[i][i+k] = max(memo[i][i+k], memo[i][i+k-m] + memo[i+k-m+1][i+k])
 
-print(memo[0][-1])
+print(memo[0][-1])  # 0부터 끝까지 글자로 만들 수 있는 최대 KOI가 출력됨
