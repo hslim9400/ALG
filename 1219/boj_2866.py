d7e0e97c4823488211c06c94a492281cd13d93c9
hslim9400 <hslim9400@gmail.com>
2022-12-23 15:21:24 +0900

    1223 1

--- 1219/boj_2866.py
+++ 1219/boj_2866.py
@@ -1,31 +1,41 @@
+# 맨 뒤에서부터 봤을때 어디까지 중복이 있는가? 를 찾아야 한다.
+# 모든 부분문자열을 해시로 저장하면 메모리제한에 걸리고,
+# 매 시행마다 문자열을 새로 만들면 시간제한에 걸린다.
 
+# 포인터를 R-1로 두고 하나씩 땡겨오면서 시행
+# 겹치는 문자열이 있는 인덱스와, 그 인덱스에서 겹치는 문자열을 저장한다. 
+# 다음 시행에서 문자열의 다음 글자를 더하고 중복되면 계속 보관, 겹치지 않으면 삭제
+# 중복되는 문자열이 없을때 까지 반복한다.
 R, C = map(int, input().split())
 words = []
 for i in range(R):
     words.append(input())
 
-idxs = list(range(C))
+idxs = list(range(C))  # 겹치는 문자열이 있는 인덱스들을 모아놓은 리스트
+# 처음엔 모두 확인할 것
 ans = R-1
-strings = {}
+strings = {}  # 겹치는 문자열을 보관
+# {인덱스 : 중복되는 문자열} 꼴의 딕셔너리 
 while ans:
-    new_strings = {}
+    new_strings = {}  # 새로운 strings를 만들기 위한 temp느낌
     for i in idxs:
         if strings:
-            base = strings[i]
+            base = strings[i]  # 지금까지의 중복된 문자열
         else:
-            base = ''
-        if base+words[ans][i] in new_strings.keys():
+            base = ''  # 첫 시행일 경우
+        if base+words[ans][i] in new_strings.keys():  
+            # 한 글자를 더 해주고 중복이 있는지 확인
             new_strings[base+words[ans][i]].append(i)
         else:
             new_strings[base+words[ans][i]] = [i]
     idxs = []
     strings = {}
     for made_word in new_strings.keys():
-        if len(new_strings[made_word]) > 1:
-            idxs.extend(new_strings[made_word])
+        if len(new_strings[made_word]) > 1:  # 중복이 있는 문자열이 있다면
+            idxs.extend(new_strings[made_word])  # 중복이 있는 인덱스도 넣고
             for idx in new_strings[made_word]:
-                strings[idx] = made_word
-    if not idxs:
+                strings[idx] = made_word  # 중복이 있는 문자열도 넣는다.
+    if not idxs:  # 중복되는 문자열이 없으면 끝
         break
     ans -= 1
 print(ans)
--- 1220/boj_3107.py
+++ 1220/boj_3107.py
@@ -1,4 +1,9 @@
+# ':'를 기준으로 문자열들을 나누고, 4글자가 되도록 0을 채워주면 
+# 일반적인 경우는 어렵지 않다.
+# 다만, ::가 숫자들 가운데 있는지, 양측 끝에 있는지에 따라 다르게 풀어야 할 수 있다.
 
+# 이 풀이는 빈칸의 인덱스를 저장해두고 그 곳에는 숫자가 없는 만큼의 '0000'을 채우고
+# 빈 칸을 모두 지운 후 위의 알고리즘을 적용했다.
 
 target = input().split(':')
 
@@ -9,14 +14,16 @@ empties = []
 for i in range(len(target)):
     if target[i] == '':
         if not found:
-            empty_idx = i
+            empty_idx = i  # '0000'이 들어갈 인덱스를 저장
             found = True
-        empties.append(i)
+        empties.append(i)  # 빈 칸을 지우기 위해 인덱스를 모은다.
     else:
         counts -= 1
 for i in empties[::-1]:
-    del target[i]
+    del target[i]  # 빈 칸을 지운다.
+
 target = target[:empty_idx] + ['0000']*counts + target[empty_idx:]
+# 들어갈 자리에 '0000'을 삽입
 for i in range(8):
     target[i] = '0'*(4-len(target[i])) + target[i]
 
