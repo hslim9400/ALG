f06c9e7695cd79c605cbb0a1e79c04969f8419c0
hslim9400 <hslim9400@gmail.com>
2023-01-14 22:49:12 +0900

    0114 1

--- 2023/0110/pg_150370.py
+++ 2023/0110/pg_150370.py
@@ -1,4 +1,21 @@
 def solution(today, terms, privacies):
     answer = []
-    
+
+    today = today.split('.')
+    today_value = int(today[0])*12*28 + int(today[1])*28 + int(today[2])
+    terms_dict = {}
+    for term in terms:
+        term_type, term_value = term.split()
+        terms_dict[term_type] = int(term_value)*28
+
+    counts = 0
+    for privacy in privacies:
+        counts += 1
+        start, term_type = privacy.split()
+        start = list(map(int, start.split('.')))
+        start_value = start[0]*12*28 + start[1]*28 + start[2]
+        end_value = start_value + terms_dict[term_type]
+        if end_value <= today_value:
+            answer.append(counts)
+
     return answer
---
+++ 2023/0114/pg_150369.py
@@ -0,0 +1,48 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    deliveries = [0] + deliveries
+    pickups = [0] + pickups
+    left_deliveries = sum(deliveries)
+    left_pickups = sum(pickups)
+    pickup_idx = 0
+    delivery_idx = 0
+
+    for i in range(n, -1, -1):
+        if not pickup_idx:
+            if pickups[i]:
+                pickup_idx = i
+        if not delivery_idx:
+            if deliveries[i]:
+                delivery_idx = i
+        if pickup_idx and delivery_idx:
+            break
+
+    while pickup_idx or delivery_idx:
+        answer += max(delivery_idx, pickup_idx)*2
+        current_deliveries = min(left_deliveries, cap)
+        current_pickups = min(left_pickups, cap)
+        left_deliveries -= current_deliveries
+        left_pickups -= current_pickups
+
+        while current_deliveries:
+            target = min(deliveries[delivery_idx], current_deliveries)
+            deliveries[delivery_idx] -= target
+            current_deliveries -= target
+            if not deliveries[delivery_idx]:
+                while delivery_idx > 0:
+                    delivery_idx -= 1
+                    if deliveries[delivery_idx]:
+                        break
+
+        while current_pickups:
+            target = min(pickups[pickup_idx], current_pickups)
+            pickups[pickup_idx] -= target
+            current_pickups -= target
+            if not pickups[pickup_idx]:
+                while pickup_idx > 0:
+                    pickup_idx -= 1
+                    if pickups[pickup_idx]:
+                        break
+    return answer
+
+print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
