5b08903e2192c9aac0c1806304a628554fcc58eb
hslim9400 <hslim9400@gmail.com>
2023-01-18 22:07:54 +0900

    0118 1

--- 2023/0117/pg_17683.py
+++ 2023/0117/pg_17683.py
@@ -1,3 +1,24 @@
+import heapq
 def solution(m, musicinfos):
     answer = ''
+    playlist = []
+    count = 0
+
+    for music in musicinfos:
+        count += 1
+        start, end, title, notes = music.split(',')
+        start_h, start_m = map(int, start.split(':'))
+        end_h, end_m = map(int, start.split(':'))
+        play_time =60*(end_h-start_h) + (end_m-start_m)
+        if play_time > len(notes):
+            played = (play_time//len(notes))*notes + notes[:(play_time-(play_time//len(notes))*notes)]
+        else:
+            played = notes[:play_time]
+        new_info = [-play_time, count, title, played]
+        heapq.heappush(playlist, new_info)
+
+    print(playlist)
+
     return answer
+
+solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])
