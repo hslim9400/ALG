ea671927d3f4f012698e803bddf88d2bda3494b5
hslim9400 <hslim9400@gmail.com>
2023-01-21 12:56:42 +0900

    0121 1

--- 2023/0119/pg_150366.py
+++ 2023/0119/pg_150366.py
@@ -2,72 +2,50 @@
 def solution(commands):
 
     def update_a(r, c, value):
-        nonlocal table, original, groups, value_map, current
-        group = groups[table[(r, c)][1]]
-        for target in group:
-            table[target][0] = value
-            if value not in value_map.keys():
-                value_map[value] = set()
-            value_map[value].add(target)
-            value_map['EMPTY'].discard(target)
+        group = table[r][c][1]
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j][1] == group:
+                    table[i][j][0] = value
 
     def update_b(value1, value2):
-        nonlocal table, original, groups, value_map, current
-        group = value_map[value1]
-        for target in group:
-            table[target][0] = value2
-            if value2 not in value_map.keys():
-                value_map[value2] = set()
-            value_map[value2].add(target)
-        value_map[value1] = set()
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j][0] == value1:
+                    table[i][j][0] = value2
 
     def merge(r_1, c_1, r_2, c_2):
-        nonlocal table, original, current, value_map, groups
-        # if (r_1, c_1) == (r_2, c_2):
-        #     return
-        group = min(table[(r_1, c_1)][1], table[(r_2, c_2)][1])
-        for target in groups[table[(r_1, c_1)][1]]:
-            current[target] = group
-            groups[group].add(target)
-            table[target][2] = group
-        for target in groups[table[(r_2, c_2)][1]]:
-            current[target] = group
-            groups[group].add(target)
-            table[target][2] = group
-
-        if table[(r_1, c_1)][0] != 'EMPTY':
-            update_a(r_2, c_2, table[(r_1, c_1)][0])
-        elif table[(r_2, c_2)][1] != 'EMPTY':
-            update_a(r_1, c_1, table[(r_2, c_2)][0])
+        group = min(table[r_1][c_1][1], table[r_2][c_2][1])
+        groups = (table[r_1][c_1][1], table[r_2][c_2][1])
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j][1] in groups:
+                    table[i][j][1] = group
+        if table[r_1][c_1][0]:
+            update_a(r_2, c_2, table[r_1][c_1][0])
+        elif table[r_2][c_2][0]:
+            update_a(r_1, c_1, table[r_2][c_2][0])
 
     def unmerge(r, c):
-        nonlocal table, original, current, value_map, groups
-        group = set(groups[table[(r, c)][1]])
-        value = table[(r, c)][0]
-        for target in group:
-            table[target][0] = 'EMPTY'
-            current[target] = original[target]
-            groups[current[target]] = {target}
-            value_map['EMPTY'].add(target)
-        table[(r, c)][0] = value
-        value_map[value] = {(r, c)}
-        value_map['EMPTY'].discard((r, c))
+        group = table[r][c][1]
+        value = table[r][c][0]
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j][1] == group:
+                    table[i][j][1] = original[i][j]
+                    table[i][j][0] = 0
+        table[r][c][0] = value
 
     answer = []
-    table = {}
-    original = {}
-    current = {}
-    groups = {}
-    value_map = {'EMPTY': set()}
+    table = [[[0, 0]] * 51 for _ in range(51)]
+    original = [[0] * 51 for _ in range(51)]
     count = 1
-    for i in range(51):
-        for j in range(51):
-            table[(i, j)] = ["EMPTY", count]
-            original[(i, j)] = count
-            current[(i, j)] = count
-            groups[count] = {(i, j)}
-            value_map['EMPTY'].add((i, j))
+    for i in range(1, 51):
+        for j in range(1, 51):
+            table[i][j] = [0, count]
+            original[i][j] = count
             count += 1
+
     for command in commands:
         command = command.split()
         if command[0] == 'UPDATE':
@@ -80,6 +58,9 @@ def solution(commands):
         if command[0] == 'UNMERGE':
             unmerge(int(command[1]), int(command[2]))
         if command[0] == 'PRINT':
-            answer.append(table[(int(command[1]), int(command[2]))][1])
+            if table[int(command[1])][int(command[2])][0]:
+                answer.append(table[int(command[1])][int(command[2])][0])
+            else:
+                answer.append("EMPTY")
 
     return answer
