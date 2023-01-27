d5e34c05a85e589d2a760f6100f111f0118df1ac
hslim9400 <hslim9400@gmail.com>
2023-01-28 00:02:01 +0900

    0128 1

--- 2023/0122/pg_42890.py
+++ 2023/0122/pg_42890.py
@@ -2,7 +2,8 @@ def solution(relation):
     answer = 0
     candidate_keys = {tuple(range(len(relation[0])))}
 
-    def find_candidate(current_cols, target):
+    def find_candidate(current_cols, target):  
+        # 현재의 속성들 중 타겟 컬럼을 빼고 식별 가능성을 검증하는 함수
         nonlocal candidate_keys
 
         is_valid = set()
@@ -14,14 +15,12 @@ def solution(relation):
             for column in new_cols:
                 new_row.append(row[column])
             new_row = tuple(new_row)
-            if new_row in is_valid:
+            if new_row in is_valid:  # 중복된다면 이 속성들로는 후보키가 될 수 없음
                 return
             is_valid.add(new_row)
-        else:
-            candidate_keys.discard(tuple(prev_cols))
-            candidate_keys.add(new_cols)
-            print(new_cols)
-            print()
+        else:  # 이 속성들로 식별 가능하다면
+            candidate_keys.discard(tuple(prev_cols))  # 내 위의 컬럼집합은 나보다 속성을 하나 더 포함하므로 후보키가 되지 못한다.
+            candidate_keys.add(new_cols)  # 그 대신 내가 새로운 후보키로 들어감
             for column in new_cols:
                 find_candidate(list(new_cols), column)
 
@@ -31,4 +30,3 @@ def solution(relation):
     answer = len(candidate_keys)
     return answer
 
-print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
