9a9ef2e722f142c19dbc150c41020bd466bd8ff4
hslim9400 <hslim9400@gmail.com>
2023-02-01 20:14:53 +0900

    0201 1

--- 2023/0129/pg_92343.py
+++ 2023/0129/pg_92343.py
@@ -1,49 +1,34 @@
-from collections import deque
 def solution(info, edges):
     answer = 0
-    children = [[] for _ in range(len(info))]
-    parents = [0 for _ in range(len(info))]
+
+    adj = [[] for _ in range(len(info))]
 
     for edge in edges:
-        children[edge[0]].append(edge[1])
+        adj[edge[0]].append(edge[1])
+        adj[edge[1]].append(edge[0])
 
-    def bfs():
-        nonlocal finish, visited, wolves, sheeps
-        queue = deque([[0, sheeps-wolves]])
-        while queue:
-            print(queue)
-            print(visited)
-            current = queue.popleft()
-            current, left_sheep = current
-            if current in visited:
-                for child in children[current]:
-                    queue.append([child, left_sheep])
-                continue
-            for child in children[current]:
-                if info[child]:
-                    if left_sheep > 2:
-                        queue.append([child, left_sheep-1])
+    visited = {0}
+
+    def explore(node, sheeps, wolves, candidates):
+        nonlocal answer, visited
+        answer = max(answer, sheeps)
+        print(node, sheeps, wolves, candidates)
+
+        new_candidates = set(candidates)
+        for adj_node in adj[node]:
+            if adj_node not in visited:
+                new_candidates.add(adj_node)
+        for destination in new_candidates:
+            if destination not in visited:
+                visited.add(destination)
+                if info[destination]:
+                    if sheeps > wolves + 1:
+                        explore(destination, sheeps, wolves+1, new_candidates)
                 else:
-                    finish = False
-                    node = child
-                    sheeps += 1
-                    visited.add(node)
-                    node = parents[node]
-                    while node not in visited:
-                        wolves += 1
-                        visited.add(node)
-                        node = parents[node]
-                    queue.append([child, left_sheep+1])
+                    explore(destination, sheeps+1, wolves, new_candidates)
+                visited.discard(destination)
 
-    finish = False
-    sheeps = 1
-    wolves = 0
-    visited = set()
-    while not finish:
-        finish = True
-        bfs()
-
-    answer = sheeps
+    explore(0, 1, 0, set(adj[0]))
     return answer
 
-print(solution([0,0,1,1,1,0,1,0,1,0,1,1], [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
+print(solution([0,1,0,1,1,0,1,0,0,1,0], [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]	))
