67f536048e784914d552dc92f9e6a2d40eab9645
hslim9400 <hslim9400@gmail.com>
2023-03-07 20:50:31 +0900

    0307 1

---
+++ 2023/0307/pg_67260.py
@@ -0,0 +1,50 @@
+from collections import deque
+def solution(n, path, order):
+    answer = True
+    left = n
+    req_dict = {}
+    next_dict = {}
+    adj = [[] for _ in range(left)]
+    for edge in path:
+        start, end = edge
+        adj[start].append(end)
+        adj[end].append(start)
+    visited = set()
+    queue = deque([0])
+    parents = {}
+    while queue:
+        current = queue.popleft()
+        if current in visited:
+            continue
+        visited.add(current)
+        for destination in adj[current]:
+            if destination not in visited:
+                parents[destination] = current
+                queue.append(destination)
+                
+    for req in order:
+        prev, after = req
+        req_dict[after] = prev
+        next_dict[prev] = after
+    queue = deque([0])
+    visited = set()
+    
+    while queue:
+        current = queue.popleft()
+        if current in req_dict.keys():
+            if req_dict[current] not in visited:
+                continue
+        if current in visited:
+            continue
+        visited.add(current)
+        left -= 1
+        for destination in adj[current]:
+            if destination not in visited:
+                queue.append(destination)
+        if current in next_dict.keys():
+            if parents[next_dict[current]] in visited:
+                queue.append(next_dict[current])
+        
+    if left:
+        answer = False
+    return answer
