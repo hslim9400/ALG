d6e5c5e689edc069e3817d2b1d49d8b1c17961b0
hslim9400 <hslim9400@gmail.com>
2023-03-09 21:32:46 +0900

    0309 1

--- 2023/0306/pg_68646.py
+++ 2023/0306/pg_68646.py
@@ -1,6 +1,7 @@
 def solution(a):
     answer = min(len(a), 2)
     possibles = [False] * len(a)
+    # 왼쪽 오른쪽을 모두 움직이며 수행
     left_min = a[0]
     for i in range(1, len(a)-1):
         number = a[i]
