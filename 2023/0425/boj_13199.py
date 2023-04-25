78024deed8b5cd0bd5e5e31507d98098a573c655
임희상 <hslim9400@gmail.com>
2023-04-25 15:59:57 +0900

    0425 1

---
+++ 2023/0425/boj_13199.py
@@ -0,0 +1,21 @@
+import sys
+T = int(input())
+
+for _ in range(T):
+  P, M, F, C = map(int, sys.stdin.readline().split())
+  
+  init_coupons = C * (M // P)
+  chicken_1 = init_coupons // F
+  chicken_2 = 0
+  current = min(init_coupons, F)
+  if init_coupons > F:
+    chicken_2 += (init_coupons-F) // (F-C)
+    current += (init_coupons-F) % (F-C)
+  
+  while current >= F:
+    chicken = current // F
+    chicken_2 += chicken
+    current -= chicken * F
+    current += chicken * C
+    
+  print(chicken_2 - chicken_1)
