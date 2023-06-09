80a9ca7244dbc7386e8a2ae4f53f7641337ff351
hslim9400 <hslim9400@gmail.com>
2023-06-09 18:58:38 +0900

    0609 1

---
+++ 2023/0609/boj_27513.py
@@ -0,0 +1,16 @@
+from collections import deque
+n, m = map(int, input().split())
+
+head = (0, 1)
+dc = [1, -1]
+d = 0
+
+queue = deque([])
+
+if not (n % 2):
+    pass
+elif not (m % 2):
+    pass
+else:
+    pass
+
