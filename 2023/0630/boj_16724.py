85e50eb727f729481de5705437cf69a0f691d43b
hslim9400 <hslim9400@gmail.com>
2023-07-02 08:06:25 +0900

    0702 1

--- 2023/0630/boj_16724.py
+++ 2023/0630/boj_16724.py
@@ -11,26 +11,21 @@ direction = {
     'L': (0, -1)
 }
 visited = [[0]*M for _ in range(N)]
-destination = set()
 answer = 0
-
-def search(r, c):
-    global answer, destination
-    while not visited[r][c]:
-        visited[r][c] = 1
-        nr = r + direction[board[r][c]][0]
-        nc = c + direction[board[r][c]][1]
-        r, c = nr, nc
-
-    if (r, c) not in destination:
-        destination.add((r, c))
-        answer += 1
-    
 
 for i in range(N):
     for j in range(M):
-        if not visited[i][j]:
-            print(i, j)
-            search(i, j)
-            print(destination)
+        if visited[i][j]:
+            continue
+
+        r, c = i, j
+        local_visited = set()
+        while not visited[r][c]:
+            visited[r][c] = 1
+            local_visited.add((r, c))
+            r += direction[board[r][c]][0]
+            c += direction[board[r][c]][1]
+        if (r, c) in local_visited:
+            answer += 1
+
 print(answer)
