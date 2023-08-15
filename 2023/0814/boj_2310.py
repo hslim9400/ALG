fe225fb2bb3cdad487d17dce17ddb7df30555218
hslim9400 <hslim9400@gmail.com>
2023-08-15 23:43:15 +0900

    0815 1

--- 2023/0814/boj_2310.py
+++ 2023/0814/boj_2310.py
@@ -1,5 +1,48 @@
 n = int(input())
+answers = []
 
-rooms = []
-for _ in range(n):
-    rooms.append(input().split())
+while n:
+
+    rooms = [0]
+    portals = [0] * (n+1)
+    for i in range(1, n+1):
+        info = input().split()
+        content, money, next_rooms = info[0], int(info[1]), list(map(int, (info[2:-1])))
+        portals[i] = next_rooms
+        rooms.append([content, money])
+    
+    visited = {}
+    if n == 1:
+        if rooms[1][1] != 'T':
+            stack = [(1, 0)]
+    else:
+        stack = [(1, 0)]
+    answer = 'No'
+
+    while stack:
+        current, money = stack.pop()
+        
+        if current == n:
+            answer = 'Yes'
+            break
+        
+        if rooms[current][0] == 'L':
+            money = max(money, rooms[current][1])
+            visited[current] = money
+        else:
+            visited[current] = money
+            money -= rooms[current][1]
+        
+        for destination in portals[current]:
+            if destination not in visited.keys() or \
+                money > visited[destination]:
+                if rooms[destination][0] == 'T' and money < rooms[destination][1]:
+                    continue
+                stack.append((destination, money))
+    
+
+    answers.append(answer)
+
+    n = int(input())
+
+print('\n'.join(answers))
