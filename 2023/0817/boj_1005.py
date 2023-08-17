35dfb97317e7345832432558e57889140076fdb8
hslim9400 <hslim9400@gmail.com>
2023-08-17 21:31:49 +0900

    0817 1

---
+++ 2023/0817/boj_1005.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for _ in range(T):
+    N, K = map(int, input().split())
+    build_times = list(map(int, input().split()))
+    for i in range(K):
+        X, Y = map(int, input().split())
+    target = int(input())
+    
