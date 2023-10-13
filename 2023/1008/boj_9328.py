54e28e546f13eec1a38ebecdf3da0143f4bbca31
hslim9400 <hslim9400@gmail.com>
2023-10-13 21:37:09 +0900

    1013 1

--- 2023/1008/boj_9328.py
+++ 2023/1008/boj_9328.py
@@ -1,1 +1,15 @@
-N = int(input())
+T = int(input())
+
+for _ in range(T):
+
+    h, w = map(int, input().split())
+
+    board = []
+    for i in range(h):
+        line = list(input())
+        board.append(line)
+
+        for j in range(w):
+            
+
+    
