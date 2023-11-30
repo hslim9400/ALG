2ed790718d1a1eeba542d414e621046b3b8127be
hslim9400 <hslim9400@gmail.com>
2023-12-01 00:08:31 +0900

    1201 1

--- 2023/1122/pg_60061.py
+++ 2023/1122/pg_60061.py
@@ -1,53 +1,62 @@
 def solution(n, build_frame):
-    def check_install_column(x, y):
-        # 기둥이 바닥에 있는 경우
-        if not y:
+    
+    def install_column(y, x):
+        if not x:
             return True
-        # 기둥이 보의 한쪽 끝 부분 위에 있는 경우
-        if (x - 1 >= 0 and arr[y][x - 1][1]) or arr[y][x][1]:
+        if (y - 1 >= 0 and board[x][y - 1][1]) or board[x][y][1]:
             return True
-        # 기둥이 다른 기둥 위에 있는 경우
-        if arr[y - 1][x][0]:
+        if board[x - 1][y][0]:
             return True
         return False
     
-    
-    def check_install_beam(x, y):
-        # 보의 한쪽 끝 부분이 기둥 위에 있는 경우
-        if arr[y - 1][x][0] or arr[y - 1][x + 1][0]:
+    def install_beam(y, x):
+        if board[x - 1][y][0] or board[x - 1][y + 1][0]:
             return True
-        # 보의 양쪽 끝 부분이 모두 다른 보와 연결되어 있는 경우
-        if x - 1 >= 0 and arr[y][x - 1][1] and arr[y][x + 1][1]:
+        if y - 1 >= 0 and board[x][y - 1][1] and board[x][y + 1][1]:
             return True
         return False
     
-    
-    def check_remove_column(x, y):
-        arr[y][x][0] = 0
-        # 왼쪽 위에 보가 있는 경우
-        if x - 1 >= 0 and arr[y + 1][x - 1][1] and not check_install_beam(x - 1, y + 1):
+    def remove_column(y, x):
+        board[x][y][0] = 0
+        if y - 1 >= 0 and board[x + 1][y - 1][1] and not install_beam(y - 1, x + 1):
             return False
-        # 오른쪽 위에 보가 있는 경우
-        if arr[y + 1][x][1] and not check_install_beam(x, y + 1):
+        if board[x + 1][y][1] and not install_beam(y, x + 1):
             return False
-        # 위에 기둥이 있는 경우
-        if arr[y + 1][x][0] and not check_install_column(x, y + 1):
+        if board[x + 1][y][0] and not install_column(y, x + 1):
             return False
         return True
         
-        
-    def check_remove_beam(x, y):
-        arr[y][x][1] = 0
-        # 양쪽 보 확인
+    def remove_beam(y, x):
+        board[x][y][1] = 0
         for i in [-1, 1]:
-            nx = x + i
-            if 0 <= nx < n and arr[y][nx][1] and not check_install_beam(nx, y):
+            ny = y + i
+            if 0 <= ny < n and board[x][ny][1] and not install_beam(ny, x):
                 return False
-        # 양쪽 기둥 확인
         for i in [0, 1]:
-            nx = x + i
-            if arr[y][nx][0] and not check_install_column(nx, y):
+            ny = y + i
+            if board[x][ny][0] and not install_column(ny, x):
                 return False
         return True
-    answer = [[1]]
+    
+    board = [[[0, 0] for _ in range(n + 1)] for _ in range(n + 1)]
+    for command in build_frame:
+        y, x, is_beam, is_installation = map(int, command)
+        if is_installation:
+            if not is_beam and install_column(y, x):
+                board[x][y][0] = 1
+            elif is_beam and install_beam(y, x):
+                board[x][y][1] = 1
+        else:  # 보
+            if not is_beam and not remove_column(y, x):
+                board[x][y][0] = 1
+            elif is_beam and not remove_beam(y, x):
+                board[x][y][1] = 1
+    
+    answer = []
+    for y in range(n + 1):
+        for x in range(n + 1):
+            for i in range(2):
+                if board[x][y][i]:
+                    answer.append([y, x, i])
+            
     return answer
